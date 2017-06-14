37b93776534f49970961dda8157d825b505f4dd9
Qifeng Huang <caoyu66@hotmail.com>
2017-06-14 22:03:18 +0100

    Missed commit

---
+++ loanapound/src/main/java/com/loanapound/db/DefaultLoanApplicationDao.java
@@ -0,0 +1,18 @@
+package com.loanapound.db;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DefaultLoanApplicationDao implements LoanApplicationDao {
+
+	public LoanApplication getLoanApplication(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean persistLoanApplication(LoanApplication loanApplication) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
