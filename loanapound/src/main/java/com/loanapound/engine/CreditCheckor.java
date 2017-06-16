9d1995cf4814e0afc43bc42a14fd40b764d27e33
Qifeng Huang <caoyu66@hotmail.com>
2017-06-16 10:59:11 +0100

    Minor code tideup

--- loanapound/src/main/java/com/loanapound/engine/CreditCheckor.java
+++ loanapound/src/main/java/com/loanapound/engine/CreditCheckor.java
@@ -14,5 +14,5 @@ public interface CreditCheckor {
 	 * @param loanApplication: full details of the loan application, with applicant details wrapped inside
 	 * @return The credit score associated with the loan applicant with this particular application. Null when it is possible to get an actual score for whatever reason
 	 */
-	public Integer getCreditScore(LoanApplication loanApplication);
+	Integer getCreditScore(LoanApplication loanApplication);
 }
